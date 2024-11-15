72a8a4bffda77c5a35daf41c11d0f937732083e7
Lucas <lucaslbmp@github.com>
2024-11-14 22:00:04 -0300

    feat: add a button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,24 @@
+import { ButtonHTMLAttributes } from "react";
+import { ButtonStyle } from "../../types";
+import "./button.css";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  btnStyle?: ButtonStyle;
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={
+        (props?.className ?? "") +
+        " app-button " +
+        (props.btnStyle ?? "primary")
+      }
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/types.tsx
@@ -0,0 +1,1 @@
+export type ButtonStyle = "primary" | "ghost" | "danger";
