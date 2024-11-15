5a5f11886539f7f1b41589a0d8e133e705646564
Lucas <lucaslbmp@github.com>
2024-11-15 19:37:21 -0300

    chore: add icons svgs

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,24 @@
+import React from "react";
+import CssSvg from "./css.svg";
+import GitSvg from "./git.svg";
+import HtmlSvg from "./html.svg";
+import JsSvg from "./javascript.svg";
+import JestSvg from "./jest.svg";
+import NextjsSvg from "./next-js.svg";
+import ReactSvg from "./react.svg";
+import TailwindSvg from "./tailwind.svg";
+import TsSvg from "./typescript.svg";
+
+const techsIcons = {
+  CssSvg,
+  GitSvg,
+  HtmlSvg,
+  JsSvg,
+  JestSvg,
+  NextjsSvg,
+  ReactSvg,
+  TailwindSvg,
+  TsSvg,
+};
+
+export default techsIcons;
